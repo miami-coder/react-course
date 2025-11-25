cb716d74d7246febab27c9daf53147f33a0ec6b8
miami-coder <defenderxz229@gmail.com>
2025-11-25 14:31:36 +0200

    2 commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
+
+function App() {
+
+  return (
+    <>
+      <MyComponent text={'Hello World!1'} />
+      <MyComponent text={'Hello World!2'} />
+      <MyComponent text={'Hello World!3'} />
+    </>
+  )
+}
+
+export default App
