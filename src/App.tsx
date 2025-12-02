2286f623f5ee6d70a55225a1dc68f898e93498f7
miami-coder <defenderxz229@gmail.com>
2025-12-02 21:02:44 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-      <MyComponent text={'Hello World!1'} />
-      <MyComponent text={'Hello World!2'} />
-      <MyComponent text={'Hello World!3'} />
+
     </>
   )
 }
--- src/components/MyComponent.tsx
+++
@@ -1,9 +0,0 @@
-import type {FC} from "react";
-
-type MyComponentPropType = {text: string}
-
-const MyComponent: FC<MyComponentPropType> = ({text}) => {
-    return <div>{text}</div>;
-}
-
-export default MyComponent;
