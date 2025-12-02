b0c988cd7522a866602baf386233a5990aa62031
miami-coder <defenderxz229@gmail.com>
2025-12-02 21:13:43 +0200

    task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
 
-function App() {
+const coursesTitleArray: string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
+const App = () => {
 
   return (
     <>
-
+        <ul>
+            {
+                coursesTitleArray.map((course, i) => <li key={i}>{course}</li>)
+            }
+        </ul>
     </>
   )
 }
+
+
 
 export default App
