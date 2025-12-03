bdb5cf49e4fb99e94293d2ee1b7b159c4c065c38
miami-coder <defenderxz229@gmail.com>
2025-12-03 13:11:28 +0200

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
+import {useState} from "react";
 
-function App() {
+const App = () => {
+    // eslint-disable-next-line prefer-const
+    let [counter, setCounter] = useState<number>(0);
 
-  return (
-    <>
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                setCounter(++counter);
+            }}>increment
+            </button>
 
-    </>
-  )
+            <button onClick={() => {
+                setCounter(--counter);
+            }}>decrement
+            </button>
+        </div>
+    )
 }
 
 export default App
